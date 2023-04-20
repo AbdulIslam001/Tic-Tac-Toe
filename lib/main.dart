60cad63ba8b5c54befe92de05c9585c7743493ba
AbdulIslam001 <111189808+AbdulIslam001@users.noreply.github.com>
2023-04-20 16:51:25 +0500

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:tic_tac_toe_game/splashScreens/startingSplashScreen.dart';
 
 // void main() {
 //   runApp(const MyApp());
 // }
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(
      const MaterialApp(
       // theme: ThemeData(
